8b282a0d84df96d9c5c1cdafdeda042683aefcb9
tasmia <tasmiaashraf0343@gmail.com>
2025-09-25 11:56:00 +0500

    fix issues

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,25 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './pages/navbar/navbar.component';
+import { HomeComponent } from "./pages/home/home.component";
+import { AboutUsComponent } from "./pages/about-us/about-us.component";
+import { ServicesComponent } from "./pages/services/services.component";
+import { FaqsComponent } from "./pages/faqs/faqs.component";
+import { ContactComponent } from "./pages/contact/contact.component";
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [NavbarComponent, HomeComponent, AboutUsComponent, ServicesComponent, FaqsComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  constructor() {
-}
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        history.replaceState(null, '', this.router.url.split('#')[0]);
+      }
+    });
+  }
 }
